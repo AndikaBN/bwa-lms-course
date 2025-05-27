bfca51ced6714df28a0c25f0682aeb8489e11b25
AndikaBN <bintangnursalih275@gmail.com>
2025-05-28 06:22:49 +0800

    feat: implement sign-in functionality and update schemas

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import userModel from "../models/userModel.js";
-import TransactionModel from "../models/transactionModel.js";
+import jwt from "jsonwebtoken";
+import transactionModel from "../models/transactionModel.js";
 
 export const signUpAction = async (req, res) => {
     const midtransUrl = process.env.MIDTRANS_URL
@@ -59,6 +60,67 @@ export const signUpAction = async (req, res) => {
             }
         })
 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+}
+
+export const signInAction = async (req, res) => {
+    try {
+        const body = req.body; // email, password
+
+        const existingUser = await userModel.findOne()
+            .where('email')
+            .equals(body.email)
+        
+        if (!existingUser) {
+            return res.status(400).json({
+                message: "User not found",
+            })
+        }
+
+        const comparePassword = bcrypt.compareSync(body.password, existingUser.password);
+
+        if (!comparePassword) {
+            return res.status(400).json({
+                message: "Email or password is incorrect",
+            })
+        }
+
+        const isValidUser = await transactionModel.findOne({
+            user: existingUser._id,
+            status: "success",
+        })
+
+        if (existingUser.role !== "student" && !isValidUser) {
+            return res.status(400).json({
+                message: "User not verified",
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: existingUser._id.toString(),
+            },
+
+            process.env.SECRET_KEY_JWT,
+            {
+                expiresIn: "1d",
+            }
+        )
+
+        return res.json({
+            message: "Sign In Success",
+            data: {
+               name: existingUser.name,
+               email: existingUser.email,
+               token,
+               role: existingUser.role,
+            }
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
