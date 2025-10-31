62ee1ace7670ea71449b95f03cd0afbfc6b5352f
AndikaBN <bintangnursalih275@gmail.com>
2025-10-31 11:22:08 +0800

    feat: add GetCategory endpoint to retrieve course categories

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -43,6 +43,22 @@ export const GetCourse = async (req, res) => {
   }
 };
 
+export const GetCategory = async (req, res) => {
+  try {
+    const categories = await categoryModel.find();
+
+    return res.json({
+      message: "Get Categories Success",
+      data: categories,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
 export const postCourse = async (req, res) => {
   try {
     const body = req.body;
