d26404c48a3b0125b8dda757d85221cc51db66ed
AndikaBN <bintangnursalih275@gmail.com>
2025-12-13 03:45:10 +0800

    feat: Refactor StudentItem import path and add initial component structure

--- src/pages/manager/course-student-list/index.jsx
+++ src/pages/manager/course-student-list/index.jsx
@@ -1,5 +1,5 @@
 import { Link, useLoaderData, useParams } from "react-router-dom";
-import StudentItem from "../student/student-item";
+import StudentItem from "./student-item";
 
 export default function ManageStudentCourseListPage() {
   const { id } = useParams();
