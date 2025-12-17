c7153b1b9939cd70734b30213cfe3137414b1883
AndikaBN <bintangnursalih275@gmail.com>
2025-12-17 09:13:48 +0800

    feat: enhance getOverviews to include course and student details with URLs

--- src/controllers/overviewController.js
+++ src/controllers/overviewController.js
@@ -1,4 +1,5 @@
 import courseModel from "../models/courseModel.js";
+import userModel from "../models/userModel.js";
 
 export const getOverviews = async (req, res) => {
   try {
@@ -47,6 +48,52 @@ export const getOverviews = async (req, res) => {
       0
     );
 
+    const coursesList = await courseModel
+      .find({
+        manager: req.user?._id,
+      })
+      .select("name thumbnail")
+      .populate({
+        path: "category",
+        select: "name -_id",
+      })
+      .populate({
+        path: "students",
+        select: "name",
+      });
+
+    const imageUrl = process.env.APP_URL + "/uploads/courses/";
+
+    const responseCourses = coursesList.map((item) => {
+      return {
+        ...item.toObject(),
+        thumbnail_url: imageUrl + item.thumbnail,
+        total_students: item.students.length,
+      };
+    });
+
+    const students = await userModel
+          .find({
+            role: "student",
+            manager: req.user._id,
+          })
+          .select("name email photo");
+    
+        const photoUrl = process.env.APP_URL + "/uploads/students/";
+    
+        const coursesData = await courseModel.find({
+          manager: req.user._id,
+        }).select("students");
+    
+        const responseStudents = students.map((item) => {
+          const studentCourses = coursesData.filter(course => course.students.includes(item._id));
+          return {
+            ...item.toObject(),
+            photo_url: photoUrl + item.photo,
+            courses: studentCourses,
+          };
+        });
+
     return res.json({
       message: "get oveviews success",
       data: {
@@ -54,6 +101,8 @@ export const getOverviews = async (req, res) => {
         totalStudents,
         totalVideos,
         totalTexts,
+        courses : responseCourses,
+        students: responseStudents,
       },
     });
   } catch (error) {
