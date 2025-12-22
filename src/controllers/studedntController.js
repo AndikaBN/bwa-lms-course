e8c0ad8565461a44a42bf135958c5dab9c1ed46f
AndikaBN <bintangnursalih275@gmail.com>
2025-12-22 23:51:35 +0800

    feat: add getStudentCourses endpoint to retrieve courses for a student

--- src/controllers/studedntController.js
+++ src/controllers/studedntController.js
@@ -16,12 +16,16 @@ export const getStudents = async (req, res) => {
 
     const imageUrl = process.env.APP_URL + "/uploads/students/";
 
-    const coursesData = await courseModel.find({
-      manager: req.user._id,
-    }).select("students");
+    const coursesData = await courseModel
+      .find({
+        manager: req.user._id,
+      })
+      .select("students");
 
     const response = students.map((item) => {
-      const studentCourses = coursesData.filter(course => course.students.includes(item._id));
+      const studentCourses = coursesData.filter((course) =>
+        course.students.includes(item._id)
+      );
       return {
         ...item.toObject(),
         photo_url: imageUrl + item.photo,
@@ -43,7 +47,7 @@ export const getStudents = async (req, res) => {
 
 export const getStudentById = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     const student = await userModel.findById(id).select("name email");
 
@@ -63,7 +67,7 @@ export const getStudentById = async (req, res) => {
       message: "Internal Server Error",
     });
   }
-}
+};
 
 export const postStudent = async (req, res) => {
   try {
@@ -203,6 +207,34 @@ export const deleteStudent = async (req, res) => {
     return res.json({
       message: "Delete Student Success",
       data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const getStudentCourses = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user._id).populate({
+      path: "courses",
+      select: "name category thumbnail",
+    });
+
+    const imageUrl = process.env.APP_URL + "/uploads/courses/";
+
+    const response = user?.courses.map((item) => {
+      return {
+        ...item.toObject(),
+        thumbnail_url: imageUrl + item.thumbnail,
+      };
+    });
+
+    return res.json({
+      message: "Get Courses by Student ID Success",
+      data: response,
     });
   } catch (error) {
     console.log(error);
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
-import { deleteStudent, getStudentById, getStudents, postStudent, updateStudent } from "../controllers/studedntController.js";
+import { deleteStudent, getStudentById, getStudentCourses, getStudents, postStudent, updateStudent } from "../controllers/studedntController.js";
 import multer from "multer";
 import { fileFilter, fileStorage } from "../utils/multer.js";
 
@@ -15,5 +15,6 @@ studentRoutes.post("/students", verifyToken, upload.single("avatar"), postStuden
 studentRoutes.put("/students/:id", verifyToken, upload.single("avatar"), updateStudent);
 studentRoutes.delete("/students/:id", verifyToken, deleteStudent);
 studentRoutes.get("/students/:id", verifyToken, getStudentById);
+studentRoutes.get("/students-courses", verifyToken, getStudentCourses);
 
 export default studentRoutes;
